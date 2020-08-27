bcf549891f6866cf2163cc8243d5af4b437091e6
MakarTch <65026668+MakarTch@users.noreply.github.com>
2020-08-27 17:26:50 -0400

    First commit

---
+++ teambankapplication/src/test/java/com/cognixia/application/TeamlibraryapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognixia.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamlibraryapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
