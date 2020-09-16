35543cc27ce037cd23b68e78e6db658143d35aa9
MakarTch <65026668+MakarTch@users.noreply.github.com>
2020-09-16 18:23:19 -0400

    Changed transaction logging to show date and time

--- teambankapplication/src/main/java/com/cognixia/application/utility/TransactionUtil.java
+++ teambankapplication/src/main/java/com/cognixia/application/utility/TransactionUtil.java
@@ -1,24 +1,36 @@
 package com.cognixia.application.utility;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class TransactionUtil {
+	
+	public static String getTime() {
+		Date date = new Date();
+		TimeZone.setDefault(TimeZone.getTimeZone("US/Eastern"));
+		SimpleDateFormat formatter = new SimpleDateFormat("E, dd MMM yyyy HH:mm z");
+		String strDate = formatter.format(date);
+		return strDate;
+	}
 
 	public static String deposit(float amount, String accountType) {
-		return "Deposited $" + amount + " to " + accountType;
+		return "Deposited $" + amount + " to " + accountType + ": " + getTime();
 	}
 	
 	public static String withdraw(float amount, String accountType) {
-		return "Withdrew $" + amount + " from " + accountType;
+		return "Withdrew $" + amount + " from " + accountType + ": " + getTime();
 	}
 	
 	public static String giveTransfer(float amount, String otherUser) {
-		return "Transferred $" + amount + " to " + otherUser;
+		return "Transferred $" + amount + " to " + otherUser + ": " + getTime();
 	}
 	
 	public static String receiveTransfer(float amount, String otherUser) {
-		return "Received $" + amount + " from " + otherUser;
+		return "Received $" + amount + " from " + otherUser + ": " + getTime();
 	}
 	
 	public static String register(float amount, String user) {
-		return "Registered account for " + user + " with initial deposit of $" + amount;
+		return "Registered account for " + user + " with initial deposit of $" + amount + ": " + getTime();
 	}
 }
